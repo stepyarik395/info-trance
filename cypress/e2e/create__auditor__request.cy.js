bb4d705d77d15b29d7d56f60993904474eb31371
User <user@mac-mini-m1-10.local>
2022-10-18 13:30:27 +0300

    fix__dynamic__id

--- cypress/e2e/create__auditor__request.cy.js
+++ cypress/e2e/create__auditor__request.cy.js
@@ -6,85 +6,62 @@ describe('login__auditor', () => {
     cy.get('.v-btn').click();
     cy.get('.row > a').click();
     //Інформація про КТЗ
-    cy.get('#input-168').type('3123441234123'); // vin
-    cy.get('#input-181').type('1994'); // Production year
-    cy.get('#input-187').click();
-    cy.get('#list-item-352-0').click();
-    cy.get('#input-192').click();
-    cy.get('#list-item-376-0').click();
-    cy.get('#input-197').click();
-    cy.get('#list-item-400-1').click();
-    cy.get('#input-202').type('400');
-    cy.get('#input-205').click();
-    cy.get('#list-item-424-1').click();
-    cy.get('#input-210').click();
-    cy.get('#list-item-433-0').click();
-    cy.get('#input-215').click();
-    cy.get('#list-item-445-0').click();
-    cy.get('.text--gray-light')
-      .contains('Нормативні акти для норми забрудьоючих речовин *')
+    cy.get('input[placeholder="VIN код"]').type('135455213');
+    cy.get('input[placeholder="Рік виробництва"]').type('1994');
+    cy.get('.v-label').contains('Марка').next().click();
+    cy.get('.v-list-item__title').contains('Audi').click();
+    cy.get('.v-label').contains('Модель').next().click();
+    cy.get('.v-list-item__title').contains('100').click();
+    cy.get('.v-label').contains('Тип палива').next().click();
+    cy.get('.v-list-item__title').contains('Бензин').click();
+    cy.get(`input[placeholder="Об'єм двигуна"]`).type('135455213');
+    cy.get('.v-label').contains('Тип трансмісії').next().click();
+    cy.get('.v-list-item__title').contains('Автоматична').click();
+    cy.get('.v-label').contains('Євро клас').next().click();
+    cy.get('.v-list-item__title').contains('EURO 6').click();
+    cy.get('.v-label').contains('Євро код').next().click();
+    cy.get('.v-list-item__title').contains('61').click();
+    cy.get('.v-label')
+      .contains('Нормативні акти для норми забрудьоючих речовин')
       .next()
       .click();
-    cy.get('.v-list-item__title').contains('ЄЕК ООН № 83-07').click();
+    cy.get('.v-list-item__title').contains('Регламент EC 715/2007 ').click();
     cy.get(
       ':nth-child(2) > .v-stepper__content > .v-stepper__wrapper > .v-card > .v-card__actions > .primary'
     ).click();
 
     //Інформація про власника
-    cy.get('#input-253').type('віовішоа');
-    cy.get('#input-256').type('йіістова');
-    cy.get('#input-259').type('Побатькові');
-    cy.get('#input-263').click();
-    cy.get(':nth-child(23)').click();
-    cy.get('tbody > :nth-child(1) > :nth-child(2) > .v-btn').click();
-    cy.get('tbody > :nth-child(1) > :nth-child(4) > .v-btn').click();
+    cy.get('input[placeholder="Прізвище"]').type('Шпак');
+    cy.get(`input[placeholder="Ім'я"]`).type('Шпак');
+    cy.get('input[placeholder="По-батькові"]').type('Шпак');
+
+    cy.get('.v-label').contains('Дата народження').next().click();
+    cy.get('.v-date-picker-years > li').contains('2000').click();
+    cy.get('.v-btn__content').contains('тра').click();
+    cy.get('.v-btn__content').contains('10').click();
+    cy.get('.v-label').contains('Клієнт не має РНОКПП (ІПН)').click();
+    cy.get('input[placeholder="Номер паспорта"]').type('364545364');
+    cy.get('.v-label').contains('Дата видачі').next().click();
+    cy.get('.v-date-picker-years > li').contains('2013').click();
+    cy.get('.v-btn__content').contains('тра').click();
     cy.get(
-      ':nth-child(6) > .v-input > .v-input__control > .v-input__slot > .v-input--selection-controls__input > .v-input--selection-controls__ripple'
-    ).click();
-    cy.get('#input-278').type('312313123');
-    cy.get('#input-282').click();
-    cy.get('.v-date-picker-years > :nth-child(10)').click();
-    cy.get('tbody > :nth-child(1) > :nth-child(2) > .v-btn').click();
-    cy.get(
-      '.menuable__content__active > .v-picker > .v-picker__body > :nth-child(1) > .v-date-picker-table > table > tbody > :nth-child(1) > :nth-child(6) > .v-btn'
-    ).click();
-    cy.get('#input-291').type('123451');
-    cy.get('#input-294').click();
-    cy.get('.text--gray-light').contains('Область').next().click();
+      '.menuable__content__active > .v-picker > .v-picker__body > :nth-child(1) > .v-date-picker-table > table > tbody > :nth-child(1) > :nth-child(4) > .v-btn'
+    )
+      .contains('1')
+      .click();
+    cy.get('input[placeholder="Індекс"]').type('5433');
+    cy.get('.v-label').contains('Область').next().click();
     cy.get('.v-list-item__title').contains('Волинська область').click();
-
-    cy.get('.text--gray-light').contains('Місто/Село *').next().click();
+    cy.get('.v-label').contains('Місто/Село').next().click();
     cy.wait(500);
-    cy.get('.v-list-item__title').contains('Нововолинськ').click();
-
-    // документи
-    cy.get('.text--gray-light')
-      .contains('Вулиця *')
-      .next()
-      .click()
-      .type('kadkwdawdok');
-    cy.get('.text--gray-light')
-      .contains('Дім *')
-      .next()
-      .click()
-      .type('kadkwdawdok');
-    cy.get('.text--gray-light')
-      .contains('Квартира ')
-      .next()
-      .click()
-      .type('kadkwdawdok');
-    cy.get('.text--gray-light')
-      .contains('Email *')
-      .next()
-      .click()
-      .type('test@mail.com');
-    cy.get('.text--gray-light')
-      .contains('Номер телефону *')
-      .next()
-      .click()
-      .type('0682176659');
+    cy.get('.v-list-item__title').contains('Луцьк').click();
+    cy.get('input[placeholder="Вулиця"]').type('5433');
+    cy.get('input[placeholder="Дім"]').type('5433');
+    cy.get('input[placeholder="Квартира"]').type('5433');
+    cy.get('input[placeholder="Email"]').type('autotest@mail.com');
+    cy.get('input[placeholder="Номер телефону"]').type('682176659');
     cy.get(
-      ':nth-child(3) > .v-stepper__content > .v-stepper__wrapper > .v-card > .v-card__actions > .primary'
+      ':nth-child(3) > .v-stepper__content > .v-stepper__wrapper > .v-card > .v-card__actions > .primary > .v-btn__content > .white--text'
     ).click();
 
     //Паспорт
